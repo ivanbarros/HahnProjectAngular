9e358390ae9f56de61dcaa260603b22d9a33d8b2
Ivan Barros <v-iBarros@lighthouseglobal.com>
2025-02-26 09:25:49 -0300

    adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EventsComponent } from "./events/events.component";
 import { RecipesComponent } from "./recipes/recipes.component";
+import { HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @Component({
   selector: 'app-root',
   standalone : true,
   imports: [RouterOutlet, EventsComponent, RecipesComponent],
+  providers: [HttpClient, BrowserAnimationsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -2,11 +2,16 @@ import { Component, OnInit, inject } from '@angular/core';
 import { NgFor, AsyncPipe} from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { forwardRef } from "@angular/core";
+registerLocaleData(localePt);
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [NgFor,AsyncPipe],
+  imports: [NgFor, AsyncPipe, forwardRef(() => FormatDateBRPipe)],
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
@@ -19,6 +24,17 @@ export class EventsComponent implements OnInit {
   }
 
   public getEvents(): Observable<any> {
-    return this.http.get('https://localhost:7105/api/Recipies');
+    return this.http.get('https://localhost:7105/api/Events');
+  }
+}
+
+
+@Pipe({
+  name: 'formatDateBR'
+})
+export class FormatDateBRPipe implements PipeTransform {
+  transform(value: any): string {
+    if (!value) return '';
+    return formatDate(value, 'dd/MM/yyyy', 'pt-BR');
   }
 }
